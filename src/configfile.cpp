523b836e61b136e68de8ec32cea6d0e427531a14
Marco Schlumpp <marco.schlumpp@gmail.com>
2012-11-09 18:51:31 +0100

    Error reporting for the parser.

--- src/configfile.cpp
+++ src/configfile.cpp
@@ -8,6 +8,7 @@
 #include <boost/spirit/include/phoenix_operator.hpp>
 #include <boost/spirit/include/phoenix_fusion.hpp>
 #include <boost/spirit/include/phoenix_container.hpp>
+#include <boost/spirit/include/phoenix_object.hpp>
 
 namespace fs = boost::filesystem;
 namespace qi = boost::spirit::qi;
@@ -15,19 +16,38 @@ namespace qi = boost::spirit::qi;
 template <typename Iterator>
 struct ConfigFileParser : qi::grammar<Iterator, std::map<std::string, ConfigItem>()>
 {
-    ConfigFileParser() : ConfigFileParser::base_type(configItems)
+    ConfigFileParser() : ConfigFileParser::base_type(configItems, "configItems")
     {
         using namespace qi::labels;
         using boost::phoenix::at_c;
         using boost::phoenix::stl::insert;
+        using boost::phoenix::val;
+        using boost::phoenix::construct;
 
         configItems = -(item % qi::eol);
         item = key                              [at_c<0>(_val) = _1]
-            >> qi::lit(':') | qi::lit('=')
-            >> value                            [at_c<1>(_val) = _1]
+            > qi::lit(':') | qi::lit('=')
+            > value                             [at_c<1>(_val) = _1]
         ;
         value = *(qi::char_ - qi::eol)          [at_c<0>(_val) += _1];
         key = *(qi::char_("A-Za-z0-9"))         [_val += _1];
+
+        configItems.name("configItems");
+        item.name("item");
+        value.name("value");
+        key.name("key");
+
+        qi::on_error<qi::fail>
+        (
+            configItems,
+            std::cerr
+                << val("Error while parsing Configuration! Expecting ")
+                << _4   // what failed
+                << val(" here: \"")
+                << construct<std::string>(_3, _2) // iterators to error-pos, end
+                << val("\"")
+                << std::endl
+        );
     }
 
     qi::rule<Iterator, std::map<std::string, ConfigItem>()> configItems;
@@ -86,6 +106,14 @@ void ConfigFile::Load()
     ConfigFileParser<std::string::const_iterator> grammar;
 
     const bool result = qi::phrase_parse(begin, end, grammar, ascii::space, this->config_);
+    if(result)
+    {
+        return;
+    }
+    else
+    {
+        std::cerr << "Error!" << std::endl;
+    }
 }
 
 void ConfigFile::Save()
