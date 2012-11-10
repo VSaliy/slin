142e9b4759a31a017db00a73af2aebc79a2b27a1
Marco Schlumpp <marco.schlumpp@gmail.com>
2012-11-10 09:12:51 +0100

    Licensed slin under ISC License.

--- src/database.hpp
+++ src/database.hpp
@@ -1,3 +1,17 @@
+// Copyright (c) 2012, Marco Schlumpp <marco.schlumpp@gmail.com>
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+// SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+// OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+// CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 #pragma once
 
 #include <string>
@@ -26,9 +40,9 @@ namespace slin
 
         void AddLink(Link &link);
         void UpdateLink(const Link &link);
-        void RemoveLink(int id); 
+        void RemoveLink(int id);
         Link GetLink(int id);
-        
+
         // Query functions
         std::set<Link> Search(std::string query);
         std::set<Link> SearchTitle(std::string query);
